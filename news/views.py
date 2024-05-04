f695cf66a37059d2eae41edf3c02bc809b43057e
Felipe Duarte <feduarte.dev@gmail.com>
2024-05-03 21:06:27 -0300

    requisito 7

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
-from news.forms import CreateCategoryForm
+from news.forms import CreateCategoryForm, CreateNewsModelForm
 from news.models import News, Category
 
 
@@ -31,6 +31,16 @@ def categories(request):
     return render(request, "categories_form.html", context)
 
 
-# Dúvida sobre tag Span
-# Dúvida sobre o CSS
-# Dúvida sobre a classe Meta para form
+def news(request):
+    form = CreateNewsModelForm()
+
+    if request.method == "POST":
+        form = CreateNewsModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            news_instance = form.save(commit=False)
+            news_instance.save()
+            form.save_m2m()
+            return redirect("home-page")
+
+    context = {"form": form}
+    return render(request, "news_form.html", context)
