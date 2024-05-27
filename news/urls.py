352c9ec17ec18ab70a5d8e406613ea7ea2e1628a
Felipe Duarte <feduarte.dev@gmail.com>
2024-05-27 16:05:36 -0300

    fixed urls

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from news.views import home, details, categories, news
+from news.views import home, details, news, CategoryViewSet
+
 
 urlpatterns = [
     path("", home, name="home-page"),
     path("news/<int:news_id>", details, name="news-details-page"),
-    path("categories/", categories, name="categories-form"),
+    path(
+        "categories/",
+        CategoryViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+                "put": "update",
+                "delete": "destroy",
+            }
+        ),
+        name="categories-form",
+    ),
     path("news/", news, name="news-form"),
 ]
